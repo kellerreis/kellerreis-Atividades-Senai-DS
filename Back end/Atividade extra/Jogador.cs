4d1445e254eea708eb224e6a3c45192fb95460fa
keller <keller.m.reis@gmail.com>
2023-05-23 17:22:43 -0300

    adicionado projeto extra

--- Back end/Atividade extra/Jogador.cs
+++ Back end/Atividade extra/Jogador.cs
@@ -1,22 +1,82 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace Atividade_extra
+public class Jogador
 {
-    public class Jogador
-    {
-        public string nome;
-        public string datadenascimento;
-        public string nacionalidade;
-        public float altura;
-        public float peso;
+    public string nome;
+    public DateTime dataNascimento;
+    public string nacionalidade;
+    public float altura;
+    public float peso;
 
-        public string VerDados()
+    public Jogador(string nome, DateTime dataNascimento, string nacionalidade, float altura, float peso)
+    {
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+        this.nacionalidade = nacionalidade;
+        this.altura = altura;
+        this.peso = peso;
+    }
+
+    public void ImprimirDados()
+    {
+        Console.WriteLine("Nome: " + nome);
+        Console.WriteLine("Data de nascimento: " + dataNascimento.ToString("dd/MM/yyyy"));
+        Console.WriteLine("Nacionalidade: " + nacionalidade);
+        Console.WriteLine("Altura: " + altura.ToString("0.00") + "m");
+        Console.WriteLine("Peso: " + peso.ToString("0.00") + "kg");
+        Console.WriteLine("Idade: " + CalcularIdade());
+        Console.WriteLine("Tempo para aposentar: " + CalcularTempoAposentar() + " anos");
+    }
+
+    public int CalcularIdade()
+    {
+        int idade = DateTime.Now.Year - dataNascimento.Year;
+        if (DateTime.Now.DayOfYear < dataNascimento.DayOfYear)
         {
-            return nome;
-            
+            idade--;
         }
+        return idade;
+    }
+
+    public int CalcularTempoAposentar();
+}
+
+public class JogadorDefesa : Jogador
+{
+    public JogadorDefesa(string nome, DateTime dataNascimento, string nacionalidade, double altura, double peso)
+        : base(nome, dataNascimento, nacionalidade, altura, peso)
+    {
+    }
+
+    public override int CalcularTempoAposentar()
+    {
+        return 40 - CalcularIdade();
     }
 }
+
+public class JogadorAtaque : Jogador
+{
+    public JogadorAtaque(string nome, DateTime dataNascimento, string nacionalidade, float altura, float peso)
+        : base(nome, dataNascimento, nacionalidade, altura, peso)
+    {
+    }
+
+    public override int CalcularTempoAposentar()
+    {
+        return 35 - CalcularIdade();
+    }
+}
+
+public class JogadorMeioCampo : Jogador
+{
+    public JogadorMeioCampo(string nome, DateTime dataNascimento, string nacionalidade, float altura, float peso)
+        : base(nome, dataNascimento, nacionalidade, altura, peso)
+    {
+    }
+
+    public override int CalcularTempoAposentar()
+    {
+        return 38 - CalcularIdade();
+    }
+}
+
