7c0a8f7b958f2e710b70efb284076312f60a12ec
keller <keller.m.reis@gmail.com>
2023-06-02 17:18:21 -0300

    continuacao do gamer

--- Back end/Atividade extra/Program.cs
+++ Back end/Atividade extra/Program.cs
@@ -1,7 +1,14 @@
 ﻿using Atividade_extra;
 
-Jogador jogador = new Jogador();
+        JogadorDefesa jogadorDefesa = new JogadorDefesa("João Silva", new DateTime(1990, 1, 1), "Brasileiro", 1.80, 80);
+        jogadorDefesa.ImprimirDados();
 
+        Console.WriteLine();
 
+        JogadorAtaque jogadorAtaque = new JogadorAtaque("Pedro Santos", new DateTime(1995, 5, 5), "Português", 1,75, 70);
+        jogadorAtaque.ImprimirDados();
 
+        Console.WriteLine();
 
+        JogadorMeioCampo jogadorMeioCampo = new JogadorMeioCampo("Juan Perez", new DateTime(1988, 10, 10), "Argentino", 1,85, 85);
+        jogadorMeioCampo.ImprimirDados();
---
+++ Projeto gamer com back e sql/Controllers/JogadorController.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Projeto_gamer_com_back_e_sql.Infra;
+
+namespace Projeto_gamer_com_back_e_sql.Controllers
+{
+    [Route("[controller]")]
+    public class JogadorController : Controller
+    {
+        private readonly ILogger<JogadorController> _logger;
+
+        public JogadorController(ILogger<JogadorController> logger)
+        {
+            _logger = logger;
+        }
+
+        Context c = new Context();
+        [Route("Listar")]
+        public IActionResult Index()
+        {
+            ViewBag.Jogador = c.Jogador.ToList();
+            ViewBag.Equipe = c.Equipe.ToList();
+
+            return View();
+        }
+
+
+        // [Route("Cadastrar")]
+
+        // public IActionResult Cadastar(IFormCollection form);
+        // {
+
+        //     //implementar o metodo de cadastro
+        // }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
